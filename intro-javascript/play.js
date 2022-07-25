8472374766f0d37e39d2b3f0009812ba916978dd
anne <annecarolinedss00@gmail.com>
2022-07-25 20:19:26 -0300

    main

---
+++ intro-javascript/play.js
@@ -0,0 +1,58 @@
+// const maca = {
+//     value: 2,
+// }
+
+// const laranja = {
+//     value: 3,
+// }
+
+
+// function mapComThis(arr, thisArg) {
+//     return arr.map(function(item){
+//         return item * this.value;
+//     }, thisArg);
+// }
+
+// const num = [1, 2];
+
+// console.log('this -> maçã', mapComThis(num, maca))]
+
+
+// function mapSemThis (arr){
+//      return arr.map(function(item){
+//         return item * 2;
+//     });
+
+// }
+
+// const nums = [2, 4, 6, 8 ,19, 23];
+
+// console.log(mapSemThis(nums));
+
+
+// function filtraPares(arr) {
+//     return arr.filter(callback);
+// }
+
+// function callback (item){
+//     return item % 2 !== 0;
+// }
+
+// const meuArray = [1, 2,3, 44, 45, 56,20, 92];
+
+// // console.log(filtraPares(meuArray))
+
+// function somaNumeros (arr) {
+//     return arr.reduce(function(prev, current) {
+//         console.log({prev});
+//         console.log({current});
+//         return prev + current;
+//     })
+// }
+
+
+// const arr = [1, 2];
+
+// console.log(somaNumeros(arr));
+
+
